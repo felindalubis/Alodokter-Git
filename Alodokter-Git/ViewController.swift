fbb5dfe2de4283e1a74fe8b00c2b07fdae3f7235
Felinda Gracia Lubis <fglubis@icloud.com>
2021-11-27 10:52:13 +0700

    Initial Commit

---
+++ Alodokter-Git/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Alodokter-Git
+//
+//  Created by Felinda Gracia Lubis on 27/11/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
